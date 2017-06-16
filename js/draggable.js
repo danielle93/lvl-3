e47d8798ed1f0459cfc65ab8ad3e261e77f41879
Danielle Roberson <danielle.roberson@icloud.com>
2017-06-16 03:18:35 -0700

    .

---
+++ js/draggable.js
@@ -0,0 +1,7 @@
+$( function() {
+    $( "#draggable1" ).draggable();
+    $( "#draggable2" ).draggable();
+    $( "#draggable3" ).draggable();
+    $( "#draggable4" ).draggable();
+    $( "#draggable5" ).draggable();
+  } );
